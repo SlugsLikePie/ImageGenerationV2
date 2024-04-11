cd0f827d12de97d0e4ebedd05a5d6b67a5c27c61
Pie <slugslikepie@gmail.com>
2024-04-10 20:28:34 -0400

    fixed sine

--- src/App.java
+++ src/App.java
@@ -2,13 +2,14 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
-        MathImage img = new MathImage(1000, 1000);
+        MathImage img = new MathImage("R.jpg");
 
-        img.mosaic(1, 10, 10, false);
-        //img.rectangularAreaColorBlur(1, 6, 4, false);
-        // img.overlayNoiseImageMultiply(1);
-        //img.cosColorAbsolute(1, false);
-        //img.sinColorAbsolute(1, false);
+        img.mosaic(1, 1, 1, false);
+        img.overlayNoiseImageMultiply(1);
+        img.rectangularAreaColorBlur(1, 6, 6, false);
+        img.sinColorAbsolute(1, false);
+        img.cosColorAbsolute(1, false);
+        img.sinColorAbsolute(1, false);
         
     }
 }
